f65c3151ee39e5f69082352c0379c51e527bc0db
hasika-sandaruwan <hasikasandaruwan.info@gmail.com>
2025-11-20 21:57:25 +0530

    completed lab report js

---
+++ model/LabTest.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const LabTestSchema = new mongoose.Schema({
+    patient:{type: mongoose.Schema.Types.ObjectId, ref:"Patient", required:true},
+    doctor:{type: mongoose.Schema.Types.ObjectId, ref:"User", required:true},
+    testName:{type: String, required:true},
+    status:{
+        type: String,
+        enum:["pending", "sample_collected", "completed", "processing"],
+        default:"pending"
+    },
+    assignedTechnician:{type: mongoose.Schema.Types.ObjectId, ref:"User"},
+    reportFile:{type: String},
+    updatedDate:{type: Date},
+},{timestamps:true});
+
+module.exports = mongoose.model("LabTest", LabTestSchema);
