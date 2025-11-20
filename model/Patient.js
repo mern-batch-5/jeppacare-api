8ee36435d6c8a3c4a3da28f6aca6f9fcdf6048a2
hasika-sandaruwan <hasikasandaruwan.info@gmail.com>
2025-11-20 21:16:25 +0530

    completed patient js

---
+++ model/Patient.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const PatientSchema = new mongoose.Schema({
+    patientId:{type: String, unique:true},
+    firstName:{type: String, required:true},
+    lastName:{type: String},
+    gender:{type: String},
+    dob:{type: Date},
+    contact:{type: String},
+    address:{type: String},
+    medicalHistory:[{type: String}],
+    documents:[
+        {fileName:String, fileUrl: String, uploadedDate:Date}
+    ]
+},{timestamps:true});
+
+module.exports = mongoose.model("patient", PatientSchema);
